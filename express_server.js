153d2899b9fdb90e9b9f9b97a9ab1513a7df03e8
Katie K <katiekucsera@gmail.com>
2023-09-27 16:58:15 -0400

    Remove console log used for testing

--- express_server.js
+++ express_server.js
@@ -24,7 +24,6 @@ app.get('/urls', (req, res) => {
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  console.log(templateVars);
   res.render('urls_show', templateVars);
 });
 
