a4c069b51cc301ac7107d2c74025a981f49c31e9
Katie K <katiekucsera@gmail.com>
2023-09-28 17:30:23 -0400

    Add get request for page to enter new URL to shorten

--- express_server.js
+++ express_server.js
@@ -3,23 +3,24 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Get requests
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
 });
 
-app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get('/urls/:id', (req, res) => {
@@ -27,10 +28,20 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+// Post requests
+
+
+// Listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
