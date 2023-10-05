90b5fb8bafb8c04e22b355d469c6fce9a3ba2abc
Katie K <katiekucsera@gmail.com>
2023-10-05 19:01:49 -0400

    Move helper functions from express_server to separate file for modularity

---
+++ helpers.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcryptjs');
+
+/**
+ * This function generates a random 6 character string to use as an ID.
+ * @returns - String containing 6 random characters.
+ */
+
+const generateRandomString = function () {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomId = '';
+  for (let i = 0; i < 6; i++) {
+    randomId += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return randomId;
+};
+
+/**
+ * This function checks if an email address is present in a particular database.
+ * @param {String} email - Email address to search for.
+ * @param {Object} database - Database to search in for email.
+ * @returns - The user object that contains the email address, if found, or null.
+ */
+
+const findUserByEmail = function (email, database) {
+  for (const userId in database) {
+    if (database[userId].email === email) {
+      return database[userId];
+    }
+  }
+  return null;
+};
+
+// Check for correct email and password combination
+/**
+ * This function check if the email and password combination entered by a user match the email and password of an existing account.
+ * @param {String} email - Email associated with user acccount.
+ * @param {String} password - Password associated with user email.
+ * @returns - The user object of the authenticated account, or false if the authentication fails.
+ */
+
+const authenticateUser = function (email, password, database) {
+  for (const userId in database) {
+    if (database[userId].email === email && bcrypt.compareSync(password, database[userId].password)) {
+      return database[userId];
+    }
+  }
+  return false;
+};
+
+module.exports = {
+  findUserByEmail,
+  authenticateUser,
+  generateRandomString
+};
